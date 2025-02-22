14b6ebe40c96f07646e6973e88f02ec91a3f52a0
arlianansrul <arliannasrul@gmail.com>
2025-02-22 20:57:17 +0700

    merapihkan layout

--- src/components/Main/AnimeDetail.jsx
+++ src/components/Main/AnimeDetail.jsx
@@ -4,16 +4,26 @@ export default function AnimeDetail({ selectedAnime }) {
     }
 
     return (
-        <div className="anime-detail">
+        <div className="anime-details">
             {selectedAnime.images && selectedAnime.images.jpg ? (
                 <img src={selectedAnime.images.jpg.image_url} alt={selectedAnime.title} />
             ) : (
                 <p>No image available</p>
             )}
-            <h2>{selectedAnime.title}</h2>
-            <p>{selectedAnime.synopsis || "No synopsis available"}</p>
-            <p>Year: {selectedAnime.year || "N/A"}</p>
-            <p>Score: {selectedAnime.score || "N/A"}</p>
+            <div className="anime-info">
+                <h2>{selectedAnime.title}</h2>
+                <div className="anime-meta">
+                    <p><strong>Rank:</strong> {selectedAnime.rank || "N/A"}</p>
+                    <p><strong>Year:</strong> {selectedAnime.year || "N/A"}</p>
+                    <p><strong>Score: </strong>{selectedAnime.score || "N/A"}</p>
+                    <p><strong>Episodes:</strong> {selectedAnime.episodes || "N/A"}</p>
+                    <p><strong>Type: </strong>{selectedAnime.type || "N/A"}</p>
+                    <p><strong>Genres:</strong> {selectedAnime.genres.map((genre) => genre.name).join(', ') || "N/A"}</p>
+                </div>
+                <p>{selectedAnime.synopsis || "No synopsis available"}</p>
+            </div>
+
+
         </div>
     );
 }
--- src/components/Main/AnimeList.jsx
+++ src/components/Main/AnimeList.jsx
@@ -1,15 +1,17 @@
 export default function AnimeList({ animes, onSelectedAnime }) {
     return (
-        <ul>
+        <ul className="anime-list">
             {animes.map((anime) => (
-                <li key={anime.mal_id} onClick={() => onSelectedAnime(anime.mal_id)}>
+                <li className="anime-detail" key={anime.mal_id} onClick={() => onSelectedAnime(anime.mal_id)}>
                     {anime.images && anime.images.jpg ? (
                         <img src={anime.images.jpg.image_url} alt={anime.title} />
                     ) : (
                         <p>No image available</p>
                     )}
-                    <h3>{anime.title}</h3>
+                    <h3 className="heading-anime">{anime.title}</h3>
                     <p>Score: {anime.score || "N/A"}</p>
+                    <p>Genres: {anime.genres.map((genre) => genre.name).join(', ') || "N/A"}</p>
+
                 </li>
             ))}
         </ul>
