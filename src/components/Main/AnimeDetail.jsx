ca07d67c01b580ad86c90e707210fd0537056737
arlianansrul <arliannasrul@gmail.com>
2025-03-09 23:13:09 +0700

    update

--- src/components/Main/AnimeDetail.jsx
+++ src/components/Main/AnimeDetail.jsx
@@ -24,11 +24,11 @@ export default function AnimeDetail({ selectedAnime, loading, error }) {
                 <h2>{selectedAnime.title}</h2>
                 <div className="anime-meta">
                     <p><strong>Rank </strong>: {selectedAnime.rank || "N/A"}</p>
-                    <p><strong>Year     </strong>: {selectedAnime.year || "N/A"}</p>
-                    <p><strong>Score    </strong>:{selectedAnime.score || "N/A"}</p>
-                    <p><strong>Episodes </strong>: {selectedAnime.episodes || "N/A"}</p>
-                    <p><strong>Type     </strong>:{selectedAnime.type || "N/A"}</p>
-                    <p><strong>Genres   </strong>: {selectedAnime.genres?.map((genre) => genre.name).join(', ') || "N/A"}</p>
+                    <p><strong>Year</strong>: {selectedAnime.year || "N/A"}</p>
+                    <p><strong>Score</strong>:{selectedAnime.score || "N/A"}</p>
+                    <p><strong>Episodes</strong>: {selectedAnime.episodes || "N/A"}</p>
+                    <p><strong>Type</strong>:{selectedAnime.type || "N/A"}</p>
+                    <p><strong>Genres</strong>: {selectedAnime.genres?.map((genre) => genre.name).join(', ') || "N/A"}</p>
                 </div>
                 <p className="synopsis">{selectedAnime.synopsis || "No synopsis available"}</p>
             </div>
